480aedcb9f4ae7074686982c43ccc5e2214d2c11
Kurt <Kurt@kurtsch.com.au>
2024-03-18 08:12:22 +1000

    corrected expansion factor calc

--- src/test_osse-ath.py
+++ src/test_osse-ath.py
@@ -2,13 +2,12 @@
 Osse via ATH formula
 Credit to the original author of Oblate Sphereoid in compression drivers, ER Geddes. 
 
-I just made a python implementation of it from below (M Batic) who has added some convenient functions including the roundover for the mouth terminations
+I just made a python implementation of it from below (M Batic) who has added some convenient functions including the roundover for the mouth terminations.
+Mu implementation is NOT EXACTLY the same as the original, but it is (hopefully) close enough for the purpose of this project. 
 https://at-horns.eu/
 
-Desmos calc
+Desmos calc of OSSE by M Batic
 https://www.desmos.com/calculator/igr6jwvi9d
-
-This is the ATH formula for the OSSE, without the expansion factor k. 
 
 
 """
@@ -20,7 +19,7 @@ L = 160  # Length of the horn
 alpha = 30.0  # Dispersion angle / 2
 r_0 = 12.7  # Throat radius
 alpha_0 = 8  # Throat angle
-k = 1  # Expansion factor ( 0 - 10)
+k = 1.8  # Expansion factor ( 0 - 10)
 s = 0.7  # Superellipse aspect ratio
 q = 0.996  # Superellipse quadrant usage factor (truncation coefficient)) 0.990 - 1.000
 n = 4.0  # superellipse exponent (compress superelipse) 2.0 - 10.0, lower numbers = lower frequency cutoff
@@ -30,13 +29,15 @@ line_coords = []  # Dispertion line coordinates
 
 for x in range(0, L + 1):
     # calculate the parts of the formula
-    throat_radius = np.power((r_0), 2)
+    throat_radius = np.power((k * r_0), 2)
     os_curve = np.power((np.tan(90 - alpha) * x), 2)
     throat_angle = (2 * k) * np.tan(90 - alpha_0) * x
+    expansion = r_0 *(1-k)
     roundover = L * s / q * (1 - np.power((1 - np.power(((q * x) / L), n)), 1 / n))
 
     # Complete formula calculations
-    y = np.sqrt(throat_radius + os_curve + throat_angle) + roundover
+    # y = np.sqrt(throat_radius + os_curve + throat_angle) + roundover
+    y = np.sqrt(throat_radius + os_curve + throat_angle) + expansion + roundover
 
     # save the points
     osse_coords.append((x, y))
